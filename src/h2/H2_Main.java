611acdf70d5cbe3790ffc9416b364fb0bbf4d51f
ReiAyanamiPlush <bozorgmehrkazemieh@gmail.com>
2025-12-01 15:34:31 +0100

    h1.5

---
+++ src/h2/H2_Main.java
@@ -0,0 +1,32 @@
+package h2;
+
+public class H2_Main {
+
+    public static void main(String[] args) {
+
+
+
+    }
+
+    public static int[] subuwu(int[] a, int[] b, int start, int end){
+
+        if (end>start){
+
+            if (a!=b){
+                int[] hor = new int[a.length];
+
+                for(int i=0; i<a.length-1; i++){
+                    hor[i] = a[i];
+                }
+                return hor;
+            }
+            else{
+
+            }
+
+        }
+
+        return new int[0];
+    }
+
+}
